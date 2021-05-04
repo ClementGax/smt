a8665bb7db5005ad7d839b0d14300493678156de
Clément <gaxottec@gmail.com>
2021-05-04 09:33:36 +0200

    shopping list

--- App.tsx
+++ App.tsx
@@ -1,21 +1,105 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
+
+interface items {
+  text: string;
+  completed: boolean;
+}
 
 export default function App() {
+  const [value, setValue] = useState<string>("");
+  const [list, setList] = useState<items[]>([]);
+
+  const handleSubmit = (): void => {
+    setList([...list, { text: value, completed: false }]);
+    setValue("");
+  };
+
+  const removeItem = (index: number): void => {
+    const newList = [...list];
+    newList.splice(index, 1);
+    setList(newList);
+  };
+
+  const toggleComplete = (index: number): void => {
+    const newList = [...list];
+    newList[index].completed = !newList[index].completed;
+    setList(newList);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>Mes courses</Text>
+      <View style={styles.search}>
+        <TextInput
+          placeholder="Que voulez-vous acheter ?"
+          value={value}
+          onChangeText={e => {
+            setValue(e);
+          }}
+          style={styles.input}
+        />
+        <Button title="Ajouter" onPress={handleSubmit} />
+      </View>
+      {list.map((item: items, index: number) => (
+        <View style={styles.listItem} key={`${index}_${item.text}`}>
+          <Text
+            style={[
+              styles.item,
+              { textDecorationLine: item.completed ? "line-through" : "none" }
+            ]}
+            onPress={() => toggleComplete(index)}
+          >
+            {item.text}
+          </Text>
+          <Button
+            title="X"
+            onPress={() => {
+              removeItem(index);
+            }}
+            color="red"
+          />
+        </View>
+      ))}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 30,
+    alignItems: "center"
   },
+  search: {
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginBottom: 20
+  },
+  input: {
+    width: 200,
+    borderColor: "black",
+    borderRadius: 5,
+    borderWidth: 1,
+    paddingLeft: 5
+  },
+  title: {
+    fontSize: 36,
+    marginBottom: 20,
+    fontWeight: "bold",
+  },
+  listItem: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    width: "100%",
+    marginBottom: 5
+  },
+  addButton: {
+    alignItems: "flex-end"
+  },
+  item: {
+    width: 250,
+    height: 35,
+  }
 });
